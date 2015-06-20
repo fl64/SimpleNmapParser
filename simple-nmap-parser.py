97d9f56d5606e73ad81a117594e7f237e4ffeba7
fl64 <flsixtyfour@gmail.com>
2015-06-20 21:01:28 +0300

    v0.1

--- simple-nmap-parser.py
+++ simple-nmap-parser.py
@@ -3,5 +3,5 @@ import xml.etree.ElementTree as ET
 tree = ET.parse('/home/user/nmap-scans/homenet.xml')
 root = tree.getroot()
 for child in root:
-        print (child.tag, child.attrib)
+        #print (child.tag, child.attrib)
         if ((child.tag == 'host') and (child.find('status').attrib['state'] == 'up')): print (child.find('address').attrib['addr'])
