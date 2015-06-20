19b1b8a41190da7f5499b794d0bbd5269d0844a5
fl64 <flsixtyfour@gmail.com>
2015-06-20 20:59:06 +0300

    v0.1

---
+++ simple-nmap-parser.py
@@ -0,0 +1,7 @@
+__author__ = 'fl64'
+import xml.etree.ElementTree as ET
+tree = ET.parse('/home/user/nmap-scans/homenet.xml')
+root = tree.getroot()
+for child in root:
+        #print (child.tag, child.attrib)
+        if ((child.tag == 'host') and (child.find('status').attrib['state'] == 'up')): print (child.find('address').attrib['addr'])
